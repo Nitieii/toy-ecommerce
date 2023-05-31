3d837c08350cca7e18c165206e40497fbe6bb7f0
Nitieii <tuananhngo2513@gmail.com>
2023-05-31 14:45:17 +0700

    to fixed the total price of cart

--- client/src/components/user/shoppingcart/cartSummator.tsx
+++ client/src/components/user/shoppingcart/cartSummator.tsx
@@ -8,13 +8,13 @@ function CartSummator() {
       <h5>Cart Total</h5>
       <ul className='summary-table'>
         <li>
-          <span>subtotal:</span> <span>${totalPrice}</span>
+          <span>subtotal:</span> <span>${totalPrice.toFixed(2)}</span>
         </li>
         <li>
           <span>delivery:</span> <span>Free</span>
         </li>
         <li>
-          <span>total:</span> <span>${totalPrice}</span>
+          <span>total:</span> <span>$ {totalPrice.toFixed(2)}</span>
         </li>
       </ul>
     </>
